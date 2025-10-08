1c187c99f833f1945eb7bae4b7517ff9831336bb
saalsabl <aisyahnzkk@gmail.com>
2025-10-08 11:14:04 +0700

    Praktikum 5

---
+++ Jobsheet5/belanja/lib/pages/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:belanja/models/item.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/item');
+              },
+              child: Card(
+                child: Container(
+                  margin: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.name,
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          'Rp ${item.price}',
+                          textAlign: TextAlign.end,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
